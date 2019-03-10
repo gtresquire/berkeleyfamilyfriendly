0576577101d10d173f7c8955f8d0ed92391bdaf9
Odysseus <arwaterman@gmail.com>
2019-03-09 20:06:06 -0800

    added script that handles connecting to firebase backend

---
+++ assets/javascript/firebase-connect.js
@@ -0,0 +1,17 @@
+// This script is for initializing the connection to the Cool Kidzzz database in firebase
+// This is where we will be storing user information, customer reviews, and location data
+// You must call this script first, before calling other scripts that require a connection
+// to firebase.
+
+
+// Initialize Firebase
+let config = {
+    apiKey: keys.FIREBASE,
+    authDomain: "cool-kids-81481.firebaseapp.com",
+    databaseURL: "https://cool-kids-81481.firebaseio.com",
+    projectId: "cool-kids-81481",
+    storageBucket: "cool-kids-81481.appspot.com",
+    messagingSenderId: "905740305108"
+  };
+
+firebase.initializeApp(config);
